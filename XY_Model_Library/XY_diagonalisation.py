f56c265a2035d27cb8e61a6a61e5d412f23bdfb5
Jose Alejandro Montana <JoseMontanaC@unal.edu.co>
2020-02-03 11:23:45 -0500

    modifications

---
+++ XY_Model_Library/XY_diagonalisation.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pylab as plt
+import pandas as pd
+import numpy as np
+import matplotlib.pylab as plt
+import pandas as pd
+from numpy.fft import fft,ifft
+from scipy.linalg import circulant,toeplitz, hankel, expm
+from matplotlib import colors
+import pickle
+#from scipy.fftpack import fft, ifft,ifftshift,fftshift
+#from IPython.display import display, HTML
